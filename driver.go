896a26d0541341e482bf7351ae1effe22636559e
wandergeek <innernetz@gmail.com>
2014-09-11 14:11:26 +1000

    first spin

---
+++ driver.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bitly/go-simplejson"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/ninjasphere/go.wemo"
+
+	"github.com/ninjasphere/go-ninja"
+	"github.com/ninjasphere/go-ninja/devices"
+)
+
+type WemoSwitchContext struct {
+	Info   *wemo.DeviceInfo
+	Switch *wemo.Device
+}
+
+func NewSwitch(bus *ninja.DriverBus, device *wemo.Device, info *wemo.DeviceInfo) (*WemoSwitchContext, error) {
+
+	log.Infof("Making Wemo switch with device info: ")
+	spew.Dump(info)
+
+	sigs, _ := simplejson.NewJson([]byte(`{
+      "ninja:manufacturer": "Belkin",
+      "ninja:productName": "Wemo",
+      "manufacturer:productModelId": "",
+      "ninja:productType": "Switch",
+      "ninja:thingType": "switch"
+  }`))
+	sigs.Set("manufacturer:productModelId", info.DeviceType)
+
+	deviceBus, err := bus.AnnounceDevice(info.SerialNumber, "wemo", info.FriendlyName, sigs)
+
+	if err != nil {
+		log.FatalError(err, "Failed to create light device bus ")
+	}
+
+	// func CreateSwitchDevice(name string, bus *ninja.DeviceBus) (*SwitchDevice, error) {
+
+	wemoSwitch, err := devices.CreateSwitchDevice(info.SerialNumber, deviceBus)
+	if err != nil {
+		log.FatalError(err, "Failed to create switch device")
+	}
+
+	if err := wemoSwitch.EnableOnOffChannel(); err != nil {
+		log.FatalError(err, "Could not enable wemo switch on-off channel")
+	}
+
+	wemoSwitch.ApplyOnOff = func(state bool) error {
+		var err error
+		if state {
+			device.On()
+		} else {
+			device.Off()
+		}
+		if err != nil {
+			return fmt.Errorf("Failed to set on-off state: %s", err)
+		}
+		return nil
+	}
+
+	ws := &WemoSwitchContext{
+		Info:   info,
+		Switch: device,
+	}
+
+	return ws, nil
+}
--- main.go
+++ main.go
@@ -2,29 +2,63 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"time"
 
-	"github.com/savaki/go.wemo"
+	"github.com/ninjasphere/go-ninja"
+	"github.com/ninjasphere/go-ninja/logger"
+	"github.com/ninjasphere/go.wemo"
 )
 
+const driverName = "driver-wemo"
+
+var log = logger.GetLogger(driverName)
+
 func main() {
-	// you can either create a device directly OR use the
-	// #Discover/#DiscoverAll methods to find devices
-	api, _ := wemo.NewByInterface("en0")
-	devices, _ := api.DiscoverAll(3 * time.Second)
-	for {
-		for _, device := range devices {
-			// device        := &wemo.Device{Host:"10.0.1.32:49153"}
 
-			// retrieve device info
-			deviceInfo, _ := device.FetchDeviceInfo()
-			fmt.Printf("Found => %+v\n", deviceInfo)
+	log.Infof("Starting " + driverName)
 
-			// device controls
-			device.On()
-			time.Sleep(1 * time.Second)
-			device.Off()
-			time.Sleep(1 * time.Second)
-		}
+	conn, err := ninja.Connect("com.ninjablocks.wemo")
+	if err != nil {
+		log.HandleError(err, "Could not connect to MQTT")
 	}
+
+	pwd, _ := os.Getwd()
+
+	bus, err := conn.AnnounceDriver("com.ninjablocks.wemo", driverName, pwd)
+	if err != nil {
+		log.HandleError(err, "Could not get driver bus")
+	}
+
+	statusJob, err := ninja.CreateStatusJob(conn, driverName)
+
+	if err != nil {
+		log.HandleError(err, "Could not setup status job")
+	}
+
+	statusJob.Start()
+
+	ipAddr, err := ninja.GetNetAddress()
+	if err != nil {
+		log.HandleError(err, "Could not get net address")
+	}
+
+	api := wemo.NewByIp(ipAddr)
+	devices, _ := api.DiscoverAll(3 * time.Second) //TODO: this needs to be evented
+	for _, device := range devices {
+		deviceInfo, err := device.FetchDeviceInfo()
+		log.HandleError(err, "Unable to fetch device info")
+		fmt.Printf("Found => %+v\n", deviceInfo)
+		_, err = NewSwitch(bus, device, deviceInfo)
+
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	// Block until a signal is received.
+	s := <-c
+	fmt.Println("Got signal:", s)
+
 }
